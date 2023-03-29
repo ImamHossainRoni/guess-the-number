c0def702a0f43219d06aa98587a58d11b862346f
imamhossainroni <imamhossainroni95@gmail.com>
2023-03-30 00:41:53 +0600

    custom component directory modified & random number generating feature added.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,25 @@
 import {StyleSheet, Text, View} from "react-native";
-import Title from "../components/Title";
-function GameScreen() {
+import Title from "../components/game/ui/Title";
+import {useState} from "react";
+import NumberContainer from "../components/game/NumberContainer";
+
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude)
+    } else {
+        return rndNum
+    }
+}
+
+function GameScreen({userNumber}) {
+    const initialGuess = generateRandomBetween(1, 100, userNumber)
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
+            <NumberContainer>{currentGuess}</NumberContainer>
             <View>
                 <Text>Higher or lower</Text>
                 {/*+*/}
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,6 @@
 import {StyleSheet, TextInput, View, Alert} from "react-native";
 import {useState} from 'react';
-import PrimaryButton from "../components/PrimaryButton";
+import PrimaryButton from "../components/game/ui/PrimaryButton";
 import Colors from "../constants/colors";
 function StartGameScreen({onPickNumber}) {
 
